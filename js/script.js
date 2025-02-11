dd21e6ba23f22ad1021a9f022b9df4487abbc529
BarbierTheo <lebarbier.lh@gmail.com>
2025-02-11 23:22:02 +0100

    comparaison des mots

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let wordLength = 6
 let length = 0
 let word = new Array()
+let wordToFind = 'dingue'
 document.addEventListener('keydown', event => {
 
     if (length <= 5 && event.key.match(/[a-zA-Z]/) && event.key.length === 1) {
@@ -9,11 +10,22 @@ document.addEventListener('keydown', event => {
         word.push(event.key)
         document.getElementById(`word`).innerText = word.join('')
     }
+
     if (event.key == "Backspace" && length >= 1) {
         document.getElementById(`letter${length}`).innerText = ''
         word.pop()
         document.getElementById(`word`).innerText = word.join('')
         length--
     }
+
+    if (length > 5) {
+
+        let compare = word.some(lettre => wordToFind.includes(lettre))
+        if (compare === true) {
+            console.log(compare)
+        }
+
+    }
+
 
 })
