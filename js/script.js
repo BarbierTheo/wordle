aaad3b0ce9d668b8b6574cafd46dba18e53fc0b4
BarbierTheo <lebarbier.lh@gmail.com>
2025-02-12 20:18:37 +0100

    changement interface, JS row

--- js/script.js
+++ js/script.js
@@ -2,30 +2,45 @@ let wordLength = 6
 let length = 0
 let word = new Array()
 let wordToFind = 'dingue'
+let row = 1
+
 document.addEventListener('keydown', event => {
 
-    if (length <= 5 && event.key.match(/[a-zA-Z]/) && event.key.length === 1) {
-        length++
-        document.getElementById(`letter${length}`).innerText = event.key
-        word.push(event.key)
-        document.getElementById(`word`).innerText = word.join('')
-    }
+    if (row < 7) {
+        if (length <= 5 && event.key.match(/[a-zA-Z]/) && event.key.length === 1) {
 
-    if (event.key == "Backspace" && length >= 1) {
-        document.getElementById(`letter${length}`).innerText = ''
-        word.pop()
-        document.getElementById(`word`).innerText = word.join('')
-        length--
-    }
-
-    if (length > 5) {
-
-        let compare = word.some(lettre => wordToFind.includes(lettre))
-        if (compare === true) {
-            console.log(compare)
+            length++
+            let parent = document.getElementById(`row${row}`)
+            let childs = parent.getElementsByClassName(`letter${length}`)
+            childs[0].innerText = event.key
+            word.push(event.key)
+            // console.log(row)
         }
 
+        if (event.key == "Backspace" && length >= 1) {
+
+
+            let parent = document.getElementById(`row${row}`)
+            let childs = parent.getElementsByClassName(`letter${length}`)
+            childs[0].innerText = ""
+            word.pop()
+            length--
+
+        }
+
+        if (length > 5) {
+
+            let compare = word.some(lettre => wordToFind.includes(lettre))
+
+            if (compare === true) {
+                console.log(compare)
+            }
+
+            row++
+            console.log(row)
+            length = 0
+
+        }
     }
-
-
 })
+
